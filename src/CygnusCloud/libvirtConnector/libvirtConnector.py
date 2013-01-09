8c99f8c2df143560484d99bdda0b83087350c9b8
saguma <saguma@gmail.com>
2013-01-09 23:08:23 +0100

    Ahora detecta cuando una maquina se apaga

--- src/CygnusCloud/libvirtConnector/libvirtConnector.py
+++ src/CygnusCloud/libvirtConnector/libvirtConnector.py
@@ -1,82 +1,95 @@
 #coding=utf-8
 
+import threading
 import libvirt
 
 class libvirtConnector():
      
     KVM = "qemu"
     Xen = "xen"
-    ESX = "esx"
     
-    def __init__(self, hypervisor, connectionMethod = "", userName = "", host = "", password = ""):
+    def __init__(self, hypervisor, eventCallbacks):
         # Creamos la uri para conectarnos 
-        # según las opciones que haya introducido
         uri = ""
         uri += hypervisor
-        if (connectionMethod != ""):
-            uri += "+"+connectionMethod
         uri += "://"
-        if (userName != ""):
-            uri += userName + "@" + host
         uri += "/system"
         # Nos conectamos a libvirt con esa uri
-        self.connector = libvirt.openAuth(uri, [[libvirt.VIR_CRED_AUTHNAME, libvirt.VIR_CRED_PASSPHRASE], self.putPassword, None], 0)
-    
-    #No se llama 
-    def putPassword(self, credentials, user_data):
-        print("eh!")
-        for credential in credentials:
-            if credential[0] == libvirt.VIR_CRED_AUTHNAME or credential[0] == libvirt.VIR_CRED_PASSPHRASE:
-                # prompt the user to input a authname. display the provided message
-                credential[4] = raw_input(credential[1] + ": ")
-    
-                # if the user just hits enter raw_input() returns an empty string.
-                # in this case return the default result through the last item of
-                # the list
-                if len(credential[4]) == 0:
-                    credential[4] = credential[3]
-            elif credential[0] == libvirt.VIR_CRED_NOECHOPROMPT:
-                # use the getpass module to prompt the user to input a password.
-                # display the provided message and return the result through the
-                # last item of the list
-                return 0
-            else:
-                return -1
-    
-        return 0
-    
-    def listDomains(self):
-        """
-        TODO: Solo devuelde los dominios definidos que no estan activos,
-        se debería usar listAllDomains, pasandole todos los flags interesantes
-        http://www.libvirt.org/html/libvirt-libvirt.html#virConnectListAllDomainsFlags
-        """
-        return self.connector.listDefinedDomains()
-        
+        self.connector = libvirt.open(uri)
+        self.connector.domainEventRegisterAny(None, 
+                                              libvirt.VIR_DOMAIN_EVENT_ID_LIFECYCLE, 
+                                              self.__eventDomain, 
+                                              None)
 
-    def listStartedDomains(self):
-        startedDomains = []
-        for domain in self.listDomains():
-            if (domain.state in self.connector.listDom):
-                startedDomains = [startedDomains, domain]
+    def __eventDomain(self, conn, domain, eventID, detail, data):
+        eventHandler = {
+         libvirt.VIR_DOMAIN_EVENT_DEFINED: self.__definedHandler,
+         libvirt.VIR_DOMAIN_EVENT_UNDEFINED: self.__undefinedHandler,
+         libvirt.VIR_DOMAIN_EVENT_STARTED: self.__startedHandler,
+         libvirt.VIR_DOMAIN_EVENT_SUSPENDED: self.__suspendedHandler,
+         libvirt.VIR_DOMAIN_EVENT_RESUMED: self.__resumedHandler,
+         libvirt.VIR_DOMAIN_EVENT_STOPPED: self.__stoppedHandler,
+         libvirt.VIR_DOMAIN_EVENT_SHUTDOWN: self.__shutdownHandler}
+        eventHandler[eventID](domain)
+        #data[eventID](domain)
         
-    """
-    Create a domain from a xml config file and start it
-    """
-    def startDomain(self, name, xmlDef = None):
-        if (xmlDef != None):
-            # TODO: Definir el dominio con el nombre que se me pasa
-            pass
-        if (name not in self.listDomains()):
-            raise 
+    def __definedHandler(self, domain):
+        pass
+    def __undefinedHandler(self, domain):
+        '''
+        Mark as free the resources in the database
+        TODO
+        '''
+        pass
+    def __startedHandler(self, domain):
+        '''
+        Start websockify for noVNC (vnc web client)
+        TODO
+        '''
+        pass
+    def __suspendedHandler(self, domain):
+        pass
+    def __resumedHandler(self, domain):
+        pass
+    def __stoppedHandler(self, domain):
+        '''
+        Undefine the domain when it is shutdown
+        '''
+        domain.undefine()
         
-        if (name in self.listStartedDomains()):
-            pass
-def main():
-    #Si se quita el comentario te pide la contraseña en vez de llamar al callback para que la devuelva
-    con = libvirtConnector(libvirtConnector.KVM #, "ssh", "saguma", "192.168.1.3"
-                           )
-    con.listDomains()
-    raw_input()
+    def __shutdownHandler(self, domain):
+        '''
+        Undefine the domain when it is shutdown
+        '''
+        domain.undefine()
     
-main() 
+    def startDomain(self, xmlConfig):
+        '''
+        Define a domain and start it
+        Args:
+            xmlConfig: Config to create domain in xml format
+        Returns:
+            Nothing
+        Raises:
+            libvirtError: if an error ocurred defining or starting the domain
+        '''
+        domain = self.connector.defineXML(xmlConfig)
+        domain.create()
+        
+    @staticmethod
+    def virEventLoopNativeRun():
+        while True:
+            libvirt.virEventRunDefaultImpl()
+            
+    @staticmethod
+    def virEventLoopNativeStart():
+        libvirt.virEventRegisterDefaultImpl()
+        eventLoopThread = threading.Thread(target=libvirtConnector.virEventLoopNativeRun, name="libvirtEventLoop")
+        eventLoopThread.setDaemon(True)
+        eventLoopThread.start()
+
+if __name__ == "__main__" :
+    libvirtConnector.virEventLoopNativeStart()
+    con = libvirtConnector(libvirtConnector.KVM,{})
+    con.startDomain(open("/home/saguma/Documentos/Universidad/config.xml", "r").read())
+    raw_input()
