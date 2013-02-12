f854bb9ed6191e1c6f98fa3958f685d9587fe7b2
Adri <adrianfernandez905@gmail.com>
2013-02-13 00:43:55 +0100

    Subo lo que he hecho hoy: "Terminada" página de arranque de máquinas para estucdiantes con gestión de la descripción actualizable y generación de hipervinculo  que redirecciona a una nueva pestaña con el cliente VNC (de momento sin meter). "Terminada" página de arranque de máquinas virtuales para el administrador  con el formulario de busqueda y la tabla de selección así como las funcionalidades descritas antes.
    	modified:   web/CygnusCloud/controllers/administrator.py
    	modified:   web/CygnusCloud/controllers/administrator.py.bak
    	modified:   web/CygnusCloud/controllers/student.py
    	modified:   web/CygnusCloud/controllers/student.py.bak
    	new file:   web/CygnusCloud/controllers/vncClient.py
    	new file:   web/CygnusCloud/controllers/vncClient.py.bak
    	modified:   web/CygnusCloud/languages/es.py
    	modified:   web/CygnusCloud/progress.log
    	new file:   web/CygnusCloud/sessions/127.0.0.1-47ea685a-ae5d-4ce8-ae22-961dcec04874
    	modified:   web/CygnusCloud/sessions/127.0.0.1-94b597fa-52b2-4481-ad0f-59b1fd37bc50
    	modified:   web/CygnusCloud/views/administrator/runVM.html
    	modified:   web/CygnusCloud/views/administrator/runVM.html.bak
    	new file:   web/CygnusCloud/views/vncClient/VNCPage.html
    	new file:   web/CygnusCloud/views/vncClient/VNCPage.html.bak

--- src/web/CygnusCloud/controllers/student.py
+++ src/web/CygnusCloud/controllers/student.py
@@ -6,7 +6,7 @@ def runVM():
     #actualizamos la barra
     createAdressBar()
     #Creamos el formulario
-    print auth.user['email']
+    print request.vars.actualDescription
     listSubjects = userDB((userDB.UserGroup.userId == auth.user['email'])).select(userDB.UserGroup.cod,userDB.UserGroup.curseGroup)
     
     tables = []
@@ -17,10 +17,17 @@ def runVM():
     i = 0
     for table in tables:
         
-        form.append(DIV(H3(T(userDB(userDB.Subjects.code == listSubjects[i].cod).select(userDB.Subjects.name)[0].name)),BR(),CENTER(table),BR()))
-    form.append(DIV(CENTER(INPUT(_type='submit',_name = 'run',  _value = T('Arrancar'))),CENTER(H3(T('Descripcion:')))))
-    return dict(form = form)
+        form.append(DIV(H4(T(userDB(userDB.Subjects.code == listSubjects[i].cod).select(userDB.Subjects.name)[0].name)),BR(),H5(table),BR()))
+    form.append(DIV(CENTER(H4(T('Descripcion:'))),CENTER(DIV(_id = 'newInfo')),BR(),CENTER(INPUT(_type='submit',_name = 'run',  _value = T('Arrancar')))))
     
+    if(form.accepts(request.vars)) and (form.vars.run):
+            if(form.vars.selection != ""):
+                #TODO: Configurar cliente VNC...
+                #Abrimos una nueva pestaña
+                form.append(CENTER(DIV(T('Máquina arrancada disponible aquí: '),A(eval(form.vars.selection)[0], 
+                _href=URL(c='vncClient',f = 'VNCPage'), _target='_blank',_select = 'selected'))))
+    return dict(form = form)
+       
    
 def createAdressBar():
     response.menu=[[T('Arrancar máquina'),False,URL('runVM')]]
@@ -32,8 +39,15 @@ def createTable(subject):
     for l in userDB((userDB.VMByGroup.cod == subject.cod) & (userDB.VMByGroup.curseGroup == subject.curseGroup) & \
         (userDB.Images.VMId == userDB.VMByGroup.VMId)).select(userDB.Images.name):
         table.append(TR(\
-        TD(INPUT(_type='radio',_name = 'selection',_value = i)),\
+        TD(INPUT(_type='radio',_name = 'selection',_value = [subject.cod , subject.curseGroup, i], _onclick = "ajax('selectRadioButton', ['selection'], 'newInfo')")),\
         TD(LABEL(l.name))))
         i = i + 1
     pass
     return table
+
+def selectRadioButton():
+    #Extraemos la descripcion actual
+    descriptionAct = userDB((userDB.VMByGroup.cod == eval(request.vars.selection)[0]) & \
+         (userDB.VMByGroup.curseGroup == eval(request.vars.selection)[1]) & \
+        (userDB.Images.VMId == userDB.VMByGroup.VMId)).select(userDB.Images.description)[eval(request.vars.selection)[2]].description
+    return descriptionAct
