48686f8ad8e801e0752391bd2179142cb8b4ec5c
Luis Barrios Hernández <lbarrios@cc.net>
2013-04-03 18:11:58 +0000

    Corrijo error en la jerarquía de gestores de constantes.
     On branch issue_45
     Changes to be committed:
    
    	renamed:    src/CygnusCloud/ccutils/configurationFiles/constantManager.py -> src/CygnusCloud/ccutils/configurationFiles/constantsManager.py
    	modified:   src/CygnusCloud/clusterServer/constants.py
    	modified:   src/CygnusCloud/virtualMachineServer/constants.py

--- src/CygnusCloud/clusterServer/constants.py
+++ src/CygnusCloud/clusterServer/constants.py
@@ -6,4 +6,4 @@ from ccutils.configurationFiles.constantManager import ConstantsManager
 class ClusterServerConstantsManager(ConstantsManager):
     def _specializeDataStructure(self):
         self._data["listenningPort"] = int(self._data["listenningPort"])
-        self._data["vmBootTimeout"] = int(self._data["vmBootTimeout"]) 
+        self._data["vmBootTimeout"] = int(self._data["vmBootTimeout"])  
--- src/CygnusCloud/virtualMachineServer/constants.py
+++ src/CygnusCloud/virtualMachineServer/constants.py
@@ -5,12 +5,13 @@ Created on 14/01/2013
 @author: saguma
 '''
 
-from ccutils.configurationFiles.constantManager import ConstantsManager
+from ccutils.configurationFiles.constantsManager import ConstantsManager
 
 class VMServerConstantsManager(ConstantsManager):
     def __init__(self):
         self._data = {}
 
     def _specializeDataStructure(self):
+        self._data["createVirtualNetworkAsRoot"] = self._data["createVirtualNetworkAsRoot"] == "Yes" or self._data["createVirtualNetworkAsRoot"] == "yes"
         self._data["listenningPort"] = int(self._data["listenningPort"])
-        self._data["vmBootTimeout"] = int(self._data["vmBootTimeout"])  
+        self._data["passwordLength"] = int(self._data["passwordLength"])            
