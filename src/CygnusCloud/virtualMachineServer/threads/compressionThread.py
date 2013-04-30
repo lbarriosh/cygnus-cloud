ec328422966c88c434d942e509f3317a91f31138
Adri <adrianfernandez905@gmail.com>
2013-04-30 03:12:52 +0200

    Subo lo poco que he conseguido 	modified:   virtualMachineServer/threads/compressionThread.py

--- src/CygnusCloud/virtualMachineServer/threads/compressionThread.py
+++ src/CygnusCloud/virtualMachineServer/threads/compressionThread.py
@@ -8,6 +8,9 @@ Created on Apr 28, 2013
 from ccutils.threads import QueueProcessingThread
 from ccutils.compression.zipBasedCompressor import ZipBasedCompressor
 from os import path
+from os import listdir
+import shutil
+from ccutils.processes.childProcessManager import ChildProcessManager
 
 class CompressionThread(QueueProcessingThread):
     def __init__(self, imageDirectory, transferDirectory, queue):
@@ -19,6 +22,21 @@ class CompressionThread(QueueProcessingThread):
         # Extraemos el fichero en el directorio de trabajo
         compressor = ZipBasedCompressor(path.join(self.__transferDirectory, str(data["SourceImageID"]) + ".zip"), "r")
         compressor.extract(path.join(self.__workingDirectory, str(data["SourceImageID"])))
+        # Movemos el fichero de definicion
+        #Buscamos el fichero con extension .xml en la ruta de descompresión
+        for files in listdir(path.join(self.__workingDirectory, str(data["SourceImageID"]))):
+            if files.endswith(".xml"):
+                defFile = files
+                break
+        #movemos el fichero al directorio
+        shutil.move(defFile, data["configFilePath"])
+        
+        #Cambiamos los permisos del resto de archivos
+        ChildProcessManager.runCommandInForeground("sudo su", Exception)
+        for files in listdir(path.join(self.__workingDirectory, str(data["SourceImageID"]))):
+            ChildProcessManager.runCommandInForeground("chmod 666 " + files, Exception)
+        ChildProcessManager.runCommandInForeground("exit", Exception)
+         
         # Arrancamos la máquina virtual
         
         # TODO: informar de errores cuando no se pueda descomprimir el fichero
